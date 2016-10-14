2b3383b49110b3e279f46d949305fa121c089ebe
soorajmenon <vn.soorajmenon@gmail.com>
2016-10-14 19:24:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
 title: 'Article-one | soorajmenon',
 heading: 'Article One',
 date:'5 sep 2016',
@@ -22,15 +22,74 @@ content:  `<p>this is article onethis is article onethis is article onethis is a
                     this is article onethis is article onethis is article onethis is article onethis is article onethis is article one</p>,`
 };
 
+function createTemplate (data) {
+    var title= data.title;
+    var heading= heading.data;
+    var date= date.data;
+    var content = content.data;
+    
+var htmlTemplate= `
+    <html>
+    <head>
+        <title>
+            
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>    
+    
+    <body>
+        <div class ="container">
+                    <div>
+                    <a href ='/'>Home</a>
+                    
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    
+                   ${date}
+                </div>
+                
+                <div>
+                    ${content}
+                </div>
+        
+            
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+    
+    
+    
+    
+</html>`;
+return htmlTemplate;
 
+}
 
-
+    
+    
+    
+    
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleOne);
 });
 
 app.get('/article-two', function (req, res) {
